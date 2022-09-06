7b985db55cd33982ff1588f0b9cb4a486d744dab
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-09-06 17:02:19 -0500

    Add google maps link

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,15 @@
 import React from 'react'
-import norway from '../assets/geirangerfjord.png'
 import drop from '../assets/drop.png'
 
 export default function Card (props) {
     return (
         <div className='card'>
-        <img src={norway} alt="" />
+        <img className="location--image" src={props.imageUrl} alt="" />
         <div className="info">
             <div className="location">
                 <img src={drop} alt=" "/>
                 <h3>{props.location}</h3>
-                <p>View on Google Maps</p>
+                <p><a href={props.googleMapsUrl}>View on Google Maps</a></p>
             </div>
             <h1>{props.title}</h1>
             <h3>{props.startDate} - {props.endDate}</h3>
