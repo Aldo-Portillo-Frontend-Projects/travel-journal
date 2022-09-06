c88bfb6d6eeb3c5972df810a4fedc69402dc1721
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-09-06 15:21:59 -0500

    Render components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-
 import './App.css';
+import Card from './components/Card';
+import Header from './components/Header'
 
 function App() {
   return (
+    <>
     <h1>It works!</h1>
+    <Header />
+    <Card />
+    </>
+    
   );
 }
 
