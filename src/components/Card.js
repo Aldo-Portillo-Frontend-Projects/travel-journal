0030e5bdf7a40d87f5379cceb3d724fb751aaf20
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-09-06 16:22:29 -0500

    Add basic card to page

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,21 @@
 import React from 'react'
+import norway from '../assets/geirangerfjord.png'
+import drop from '../assets/drop.png'
 
 export default function Card () {
     return (
-        <h1>Card Component Works</h1>
+        <div className='card'>
+        <img src={norway} alt="" />
+        <div className="info">
+            <div className="location">
+                <img src={drop} alt=" "/>
+                <h3>Norway</h3>
+                <p>View on Google Maps</p>
+            </div>
+            <h1>Geirangerfjord</h1>
+            <h3>01 Oct, 2021 - 18 Nov, 2021</h3>
+            <p>The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.</p>
+        </div>
+        </div>
     )
 }
